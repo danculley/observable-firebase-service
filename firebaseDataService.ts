572b45967a937178ab7ad29a6b510fe7f0fb09e1
danculley <danculley@gmail.com>
2016-01-04 18:33:50 -0500

    Simplified, now using only value event for arrays
    After doing some anecdotal performance testing, I did not see much difference between using the value event for arrays each time the array is updated, versus keeping a reference copy of the array and mutating it on child_ events. I also refactored to use Observable.fromEventPattern to use a more functional style, which is much terser.

--- firebaseDataService.ts
+++ firebaseDataService.ts
@@ -1,117 +1,26 @@
-import {Observable} from 'rxjs/Observable';
-
-// Helper method for transforming the key/value pair from a snapshot into an object
-function _createRecord(key, value) {
-  var record = {};
-  if (typeof value === 'object' && value !== null) {
-    record = value;
-  } else {
-    record['.value'] = value;
-  }
-  record['.key'] = key;
-
-  return record;
-}
+/* From https://github.com/danculley/observable-firebase-service */
+import Rx from 'rxjs/Rx';
 
 export class FirebaseDataArray {
   items:Observable;
   
   constructor(public ref:string) {
 
-    this.items = new Observable(observer => {
-      let data = [];
-      let doneWithInitialLoading = false;
-      
-      // Set up bindings relevant to firebase arrays
-      this.ref.on('child_added', child_added, error);
-      this.ref.on('child_removed', child_removed, error);
-      this.ref.on('child_changed', child_changed, error);
-      this.ref.on('child_moved', child_moved, error);
-      
-      // Set up a one-time binding to value to know when we're current with the existing database
-      this.ref.once('value', value, error);
-      
-      function error(err) {
-        observer.error(err);
-      }
-      
-      // Once we're current with the existing database, emit the first value
-      function value(snapshot) {
-        doneWithInitialLoading = true;
-        observer.next(data);
-      }
-      
-      function child_added(snapshot, previousChildKey) {
-        // Determine where to insert the new record
-        var insertionIndex;
-        if (previousChildKey === null) {
-          insertionIndex = 0;
-        } else {
-          let previousChildIndex = data.findIndex(e => e['.key'] === previousChildKey);
-          insertionIndex = previousChildIndex + 1;
-        }
-        
-        // Add the new record to the array
-        data.splice(insertionIndex, 0, _createRecord(snapshot.key(), snapshot.val()));
-        
-        // Emit the state from the observable if we're done with initial loading
-        if (doneWithInitialLoading)
-          observer.next(data);
-      }
-      
-      function child_removed(snapshot) {
-         // Look up the record's index in the array
-        var index = data.findIndex(e => e['.key'] === snapshot.key());
-      
-        // Splice out the record from the array
-        data.splice(index, 1); 
-        
-        // Emit the state from the observable
-        observer.next(data);
-      }
-      
-      function child_changed(snapshot) {
-        // Look up the record's index in the array
-        var index = data.findIndex(e => e['.key'] === snapshot.key());
-      
-        // Update the record's value in the array
-        data[index] = _createRecord(snapshot.key(), snapshot.val());
-        
-        // Emit the state from the observable
-        observer.next(data);
-      }
-      
-      function child_moved(snapshot, previousChildKey) {
-        // Look up the record's index in the array
-        var currentIndex = data.findIndex(e => e['.key'] === snapshot.key());
-      
-        // Splice out the record from the array
-        var record = data.splice(currentIndex, 1)[0];
-      
-        // Determine where to re-insert the record
-        var insertionIndex;
-        if (previousChildKey === null) {
-          insertionIndex = 0;
-        } else {
-          let previousChildIndex = data.findIndex(e => e['.key'] === previousChildKey);
-          insertionIndex = previousChildIndex + 1;
-        }
-      
-        // Re-insert the record into the array in the correct location
-        data.splice(insertionIndex, 0, record);
-        
-        // Emit the state from the observable
-        observer.next(data);
-      }
-
-      // Return a function that tears down the bindings
-      return () => {
-        this.ref.off('child_added', child_added);
-        this.ref.off('child_removed', child_removed);
-        this.ref.off('child_changed', child_changed);
-        this.ref.off('child_moved', child_moved);
-      };
-    });
+    this.items = Rx.Observable.fromEventPattern(
+      _ => this.ref.on('value', _),
+      _ => this.ref.off('value', _)
+      )
+      .map(snapshot => snapshot.val())
+      .map(value => {
+        return Object
+          .keys(value)
+          .map(key => {
+            if (typeof value[key] === 'object' && value !== null)
+              return Object.assign({'.key':key}, value[key])
+            else
+              return {'.key':key, '.value':value[key]}
+          })
+      }) 
   }
   
   // Add a new item to the array
@@ -141,26 +50,17 @@ export class FirebaseDataObject {
   value:Observable;
   
   constructor(public ref:string, public cancelCallback) {
-    this.value = new Observable(observer => {
-      let data = {};
-      
-      //setup firebasebindings
-      ref.on('value', value, error);
-      
-      function error(err) {
-        observer.error(err);
-      }
-      
-      function value(snapshot) {
-        // Create a new record from the snapshot
-        data = _createRecord(snapshot.key(), snapshot.val());
-        
-        // Emit the state from the observable
-        observer.next(data);
-      }
-      
-      return () => this.ref.off('value', value);
-    });
+    this.value = Rx.Observable.fromEventPattern(
+      _ => this.ref.on('value', _),
+      _ => this.ref.off('value', _)
+      )
+      .map(snapshot => {
+        if (typeof snapshot.val() === 'object' && snapshot.val() !== null)
+            return Object.assign({'.key':snapshot.key()}, snapshot.val())
+          else
+            return {'.key':snapshot.key(), '.value':snapshot.val()}
+      })
+    
   }
   
   // Modify the item
